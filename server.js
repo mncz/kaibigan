be03cef79cffb9566a8a01a9653b7c879ea00ced
Charlie <73146947+mncz@users.noreply.github.com>
2023-11-03 17:57:29 +0100

    Class correction, staticPath function

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const port = 3000
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
-app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap/dist/css')))
-app.use('/css', express.static(path.join(__dirname, './node_modules/animate.css/')))
-app.use('/css', express.static(path.join(__dirname, './public/css')))
-app.use('/js', express.static(path.join(__dirname, './node_modules/@popperjs/core/dist/umd')))
-app.use('/js', express.static(path.join(__dirname, './node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, './node_modules/jquery/dist')))
-app.use('/img', express.static(path.join(__dirname, './public/img')))
+app.use('/css', staticPath('./node_modules/bootstrap/dist/css'))
+app.use('/css', staticPath('./node_modules/animate.css/'))
+app.use('/css', staticPath('./public/css'))
+app.use('/js', staticPath('./node_modules/@popperjs/core/dist/umd'))
+app.use('/js', staticPath('./node_modules/bootstrap/dist/js'))
+app.use('/js', staticPath('./node_modules/jquery/dist'))
+app.use('/img', staticPath('./public/img'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
@@ -28,3 +28,7 @@ app.post('/signin', (req, res) => {
 app.listen(port, (err) => {
     console.log(`Listening on port ${port}…`)
 })
+
+function staticPath(url) {
+    return express.static(path.join(__dirname, url))
+}
